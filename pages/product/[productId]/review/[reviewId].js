253c616e7dcbb8d91d02f51208ca0747c1e3ce0d
Sharukh <sharukhsm@gmail.com>
2022-03-31 13:22:29 +0530

    dynamic nested routes

---
+++ pages/product/[productId]/review/[reviewId].js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const Review = () => {
+  const router = useRouter();
+  const { productId, reviewId } = router.query;
+
+  return (
+    <div>
+      <h3>
+        Review {reviewId} for product {productId}
+      </h3>
+    </div>
+  );
+};
+
+export default Review;
